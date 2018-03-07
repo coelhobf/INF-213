bad5e270313037754c57848c396fb2cdcb7f57cf
NoBugsAndroid <fabio.martins@nobugs.com.br>
2018-03-07 15:48:46 -0300

    ItemAgenda

---
+++ ItemAgenda.cpp
@@ -0,0 +1,46 @@
+#include "ItemAgenda.h"
+
+ItemAgenda::ItemAgenda()
+{
+
+}
+
+ItemAgenda::ItemAgenda(const string &descricao, const Data &data, const Horario &horario)
+{
+
+}
+
+ItemAgenda::ItemAgenda(const ItemAgenda &itemAgenda)
+{
+
+}
+
+void ItemAgenda::setDescricao(const string &descricao)
+{
+
+}
+
+void ItemAgenda::setData(const Data &data)
+{
+
+}
+
+void ItemAgenda::setHorario(const Horario &horario)
+{
+
+}
+
+const string& ItemAgenda::getDescricao()
+{
+
+}
+
+const Data& ItemAgenda::getData()
+{
+
+}
+
+const Horario& ItemAgenda::getHorario()
+{
+    
+}
